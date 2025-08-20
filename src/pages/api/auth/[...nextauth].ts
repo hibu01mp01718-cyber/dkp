52ef0cf1aec8aff7f68a900d7461c408b4db1a4c
hibu01mp01718-cyber <hibu01.mp01718@gmail.com>
2025-08-20 09:18:07 +0800

    Main

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '@/lib/mongodb';
+
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
